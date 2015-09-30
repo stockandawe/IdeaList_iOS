0a5fcdf059e1c94084a404ffe4cf152e2c0b50b4
Rutul Dave <rutul98@yahoo.com>
2015-09-30 15:14:47 -0700

    Enable/disable 'Done' and "Add' buttons based on the text input fields

--- Idea List/AddViewController.swift
+++ Idea List/AddViewController.swift
@@ -12,11 +12,17 @@ class AddViewController: UIViewController {
 
     @IBOutlet weak var titleText: UITextField!
     @IBOutlet weak var detailContent: UITextView!
-    
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    @IBOutlet weak var addButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "textTitleDidChange", name: UITextFieldTextDidChangeNotification, object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "detailContentDidChange", name: UITextViewTextDidChangeNotification, object: nil)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,12 +30,41 @@ class AddViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func textTitleDidChange() {
+        handleButtonStates()
+    }
+
+    func detailContentDidChange() {
+        handleButtonStates()
+    }
+
     @IBAction func doneButton_click(sender: AnyObject) {
         detailContent.resignFirstResponder()
     }
     
     @IBAction func titleDoneButton_click(sender: AnyObject) {
         titleText.resignFirstResponder()
+    }
+    
+    func handleButtonStates () {
+        // handle 'Done' button
+        if (detailContent.text != "") {
+            doneButton.enabled = true
+        }
+        else {
+            doneButton.enabled = false
+        }
+        
+        // handle 'Add' button
+        if (titleText.text != "" && detailContent.text != "") {
+            addButton.enabled = true
+            addButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+        }
+        else {
+            addButton.enabled = false
+            addButton.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Normal)
+
+        }
     }
 
     /*
